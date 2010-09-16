c891f18cf43426e198d9257330f71da99ed69c95
Gavin Carr <gavin@openfusion.com.au>
2010-09-16 13:55:32 +0100

    Add initial Net::SSHR::Formatter class.

---
+++ lib/net/sshr/formatter.rb
@@ -0,0 +1,38 @@
+# Net::SSHR::Formatter class
+
+module Net
+  class SSHR::Formatter
+    def initialize(options)
+      @options = options
+    end
+
+    # Render the given result
+    def render(res)
+      res[:stdout] ||= ''
+      res[:stdout].chomp!
+      res[:stderr] ||= ''
+      res[:stderr].chomp!
+
+      # Smart default formatter: use short for single lines, otherwise long
+      @options[:fmt] ||= res[:stdout] =~ /\n/ ? 'long' : 'short'
+
+      self.method(@options[:fmt]).call(res)
+    end
+
+    # Long output renderer
+    def long(res)
+      puts "[#{res[:host]}]"
+      puts res[:stdout] if res[:stdout] != ''
+      puts if res[:stdout] != '' and res[:stderr] != ''
+      puts res[:stderr] if res[:stderr] != ''
+      puts
+    end
+
+    # Short output renderer
+    def short(res)
+      format = "%-20.20s %s\n"
+      printf format, res[:host] + ':', res[:stdout] if res[:stdout] != ''
+    end
+  end
+end
+
