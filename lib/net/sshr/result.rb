8ccf3dead69a53e8765370b7a3c063560ededeb9
Gavin Carr <gavin@openfusion.com.au>
2010-09-27 17:24:38 +0100

    Revert Result to module/class hierarchy; remove append methods; add rdocs.

--- lib/net/sshr/result.rb
+++ lib/net/sshr/result.rb
@@ -1,27 +1,43 @@
-# Net::SSHR::Result class, modelling an individual host result
 
-class Net::SSHR::Result
-  attr_accessor :host, :stdout, :stderr, :exit_code
-  def initialize(host, stdout = '', stderr = '', exit_code = '')
-    @host = host
-    @stdout = stdout
-    @stderr = stderr
-    @exit_code = exit_code
-  end
-  def append_stdout(string)
-    @stdout += string
-  end
-  def append_stderr(string)
-    @stderr += string
-  end
-  def to_json(*a)
-    {
-      'json_class'  => self.class.name,
-      'host'        => @host,
-      'stdout'      => @stdout,
-      'stderr'      => @stderr,
-      'exit_code'   => @exit_code,
-    }.to_json(*a)
+module Net
+  class SSHR
+
+    # = ABSTRACT
+    #
+    # Net::SSHR::Result class, encapsulating the result data from an individual host
+    #
+    # = SYNOPSIS
+    #
+    #   require 'net/sshr/result'
+    #
+    #   result = Net::SSHR::Result.new('myhost')
+    #   result.stdout << "Hello World!\n"
+    #
+    #   puts "#{result.host}: #{result.exit_code}"
+    #   puts result.to_json
+    #
+    class Result
+      attr_accessor :host, :stdout, :stderr, :exit_code
+
+      def initialize(host, stdout = '', stderr = '', exit_code = nil)
+        @host = host
+        @stdout = stdout
+        @stderr = stderr
+        @exit_code = exit_code
+      end
+
+      # Return current result data as a serialised json hash, with hash members
+      # named after attributes
+      def to_json(*a)
+        {
+          'json_class'  => self.class.name,
+          'host'        => @host,
+          'stdout'      => @stdout,
+          'stderr'      => @stderr,
+          'exit_code'   => @exit_code,
+        }.to_json(*a)
+      end
+    end
   end
 end
 
