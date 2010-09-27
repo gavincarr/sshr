fb5dd0853aaa9feb1098a8c1e63c6426d5112161
Gavin Carr <gavin@openfusion.com.au>
2010-09-27 18:53:59 +0100

    Update Net::SSHR::Merger to module/class hierarchy style.

--- lib/net/sshr/merger.rb
+++ lib/net/sshr/merger.rb
@@ -1,22 +1,27 @@
-# Net::SSHR::Merger class, collating and merging result sets
 
-class Net::SSHR::Merger < Array
-  def merge(res)
-    res[:stdout] ||= ''
-    res[:stdout].chomp!
-    res[:stderr] ||= ''
-    res[:stderr].chomp!
+module Net
+  class SSHR
 
-    added = false
-    each do |merged|
-      if merged[:stdout] == res[:stdout] and merged[:stderr] == res[:stderr]:
-        merged[:host] += " #{res[:host]}"
-        added = true
-        break
+    # Net::SSHR::Merger class, collating and merging result sets
+    class Merger < Array
+      def merge(result)
+        res[:stdout] ||= ''
+        res[:stdout].chomp!
+        res[:stderr] ||= ''
+        res[:stderr].chomp!
+
+        added = false
+        each do |merged|
+          if merged[:stdout] == res[:stdout] and merged[:stderr] == res[:stderr]:
+            merged[:host] += " #{res[:host]}"
+            added = true
+            break
+          end
+        end
+
+        push res if not added
       end
     end
-
-    push res if not added
   end
 end
 
