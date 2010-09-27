faa8991c51b1e8dd706a0c6287af0f9aff2b8e6f
Gavin Carr <gavin@openfusion.com.au>
2010-09-27 15:42:13 +0100

    Change Result class declaration to the simpler 'class Net::SSHR::Result'.

--- lib/net/sshr/result.rb
+++ lib/net/sshr/result.rb
@@ -1,31 +1,27 @@
 # Net::SSHR::Result class, modelling an individual host result
 
-module Net
-  class SSHR
-    class Result
-      attr_accessor :host, :stdout, :stderr, :exit_code
-      def initialize(host, stdout = '', stderr = '', exit_code = '')
-        @host = host
-        @stdout = stdout
-        @stderr = stderr
-        @exit_code = exit_code
-      end
-      def append_stdout(string)
-        @stdout += string
-      end
-      def append_stderr(string)
-        @stderr += string
-      end
-      def to_json(*a)
-        {
-          'json_class'  => self.class.name,
-          'host'        => @host,
-          'stdout'      => @stdout,
-          'stderr'      => @stderr,
-          'exit_code'   => @exit_code,
-        }.to_json(*a)
-      end
-    end
+class Net::SSHR::Result
+  attr_accessor :host, :stdout, :stderr, :exit_code
+  def initialize(host, stdout = '', stderr = '', exit_code = '')
+    @host = host
+    @stdout = stdout
+    @stderr = stderr
+    @exit_code = exit_code
+  end
+  def append_stdout(string)
+    @stdout += string
+  end
+  def append_stderr(string)
+    @stderr += string
+  end
+  def to_json(*a)
+    {
+      'json_class'  => self.class.name,
+      'host'        => @host,
+      'stdout'      => @stdout,
+      'stderr'      => @stderr,
+      'exit_code'   => @exit_code,
+    }.to_json(*a)
   end
 end
 
