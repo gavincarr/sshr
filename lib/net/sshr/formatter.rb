67279f08c8e5c7cc0d5878c02fb97e6e67ce89a4
Gavin Carr <gavin@openfusion.com.au>
2010-09-27 18:47:31 +0100

    Update Formatter to use positional params in constructor and module/class.

--- lib/net/sshr/formatter.rb
+++ lib/net/sshr/formatter.rb
@@ -1,87 +1,94 @@
-# Net::SSHR::Formatter class, handling formatting of Net::SSHR::Results
 
-class Net::SSHR::Formatter
-  def initialize(options)
-    @options = { :hostwidth => 20 }     # defaults
-    @options.merge!(options)
-  end
+module Net
+  class SSHR
 
-  # Render the given result
-  def render(res)
-    res.stdout.chomp!
-    res.stderr.chomp!
+    # Net::SSHR::Formatter class, handling formatting of Net::SSHR::Results
+    class Formatter
+      def initialize(format = nil, out_err_selector = nil, annotate_flag = false, hostwidth = 20)
+        @format = format
+        @out_err_selector = out_err_selector
+        @annotate_flag = annotate_flag
+        @hostwidth = hostwidth
+      end
 
-    # Default formatter: use short for single lines, otherwise long
-    @options[:fmt] ||= res.stdout =~ /\n/ ? :long : :short
+      # Render the given result
+      def render(result)
+        result.stdout.chomp!
+        result.stderr.chomp!
 
-    # Default oe_selector: both stdout/stderr in long mode, stdout or stderr in short
-    @options[:oe_selector] ||= @options[:fmt] == :long ? :oe_b : :oe_x; 
+        # Default formatter: use short for single lines, otherwise long
+        @format ||= result.stdout =~ /\n/ ? :long : :short
 
-    method(@options[:fmt]).call(res)
-  end
+        # Default out_err_selector: stdout xor stderr in 'short' mode, otherwise both
+        @out_err_selector ||= (@format == :short ? :oe_xor : :oe_both)
 
-  # Render the given set of results
-  def render_all(res_set)
-    # If we're doing the whole set and :fmt is :short, adapt hostwidth
-    @options[:hostwidth] = 1
-    res_set.each do |res|
-      res_hostwidth = res.host.length + 2
-      if @options[:hostwidth] < res_hostwidth
-         @options[:hostwidth] = res_hostwidth
+        method(@format).call(result)
+      end
+
+      # Render the given set of results
+      def render_all(result_set)
+        # If we're doing the whole set and :format is :short, adapt hostwidth
+        @hostwidth = 1
+        result_set.each do |result|
+          result_hostwidth = result.host.length + 2
+          if @hostwidth < result_hostwidth
+             @hostwidth = result_hostwidth
+          end
+        end
+
+        # Render each result
+        result_set.each { |result| render(result) }
+      end
+
+      def display_stdout(stdout)
+        return false if not stdout
+        return false if @out_err_selector == :oe_err
+        return true
+      end
+
+      def display_stderr(stderr, stdout)
+        return false if not stderr
+        return false if @out_err_selector == :oe_out
+        return false if @out_err_selector == :oe_xor and stdout
+        return true
+      end
+
+      private
+
+      # Long output renderer
+      def long(result)
+        display_stdout = display_stdout(result.stdout)
+        display_stderr = display_stderr(result.stderr, result.stdout)
+
+        puts "[#{result.host}]"
+        puts result.stdout if display_stdout
+        if display_stdout and display_stderr:
+          puts 
+          puts '** STDERR **' if @annotate_flag
+        end
+        puts result.stderr if display_stderr
+        puts
+      end
+
+      # Short output renderer
+      def short(result)
+        fmt = "%-#{@hostwidth}s %s%s\n"
+        if display_stdout(result.stdout):
+          stdout = result.stdout.sub(/\n.*/m, '')
+          printf fmt, result.host + ':', @annotate_flag ? '[O] ' : '', stdout
+        end
+        if display_stderr(result.stderr, result.stdout):
+          stderr = result.stderr.sub(/\n.*/m, '')
+          printf fmt, result.host + ':', @annotate_flag ? '[E] ' : '', stderr
+        end
+      end
+
+      # JSON renderer
+      def json(result)
+        require 'json'
+        puts result.to_json
       end
     end
-
-    # Render each res
-    res_set.each { |res| render(res) }
-  end
-
-  def display_stdout(stdout)
-    return false if stdout == ''
-    return false if @options[:oe_selector] == :oe_e
-    return true
-  end
-
-  def display_stderr(stderr, stdout)
-    return false if stderr == ''
-    return false if @options[:oe_selector] == :oe_o
-    return false if @options[:oe_selector] == :oe_x and stdout != ''
-    return true
-  end
-
-  private
-
-  # Long output renderer
-  def long(res)
-    display_stdout = display_stdout(res.stdout)
-    display_stderr = display_stderr(res.stderr, res.stdout)
-
-    puts "[#{res.host}]"
-    puts res.stdout if display_stdout
-    if display_stdout and display_stderr:
-      puts 
-      puts '** STDERR **' if @options[:stream]
-    end
-    puts res.stderr if display_stderr
-    puts
-  end
-
-  # Short output renderer
-  def short(res)
-    fmt = "%-#{@options[:hostwidth]}s %s%s\n"
-    if display_stdout(res.stdout):
-      stdout = res.stdout.sub(/\n.*/m, '')
-      printf fmt, res.host + ':', @options[:stream] ? '[O] ' : '', stdout
-    end
-    if display_stderr(res.stderr, res.stdout):
-      stderr = res.stderr.sub(/\n.*/m, '')
-      printf fmt, res.host + ':', @options[:stream] ? '[E] ' : '', stderr
-    end
-  end
-
-  # JSON renderer
-  def json(res)
-    require 'json'
-    puts res.to_json
   end
 end
 
