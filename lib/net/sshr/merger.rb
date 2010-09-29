feb2da1fefbd833d40a9b75accde162f4d6af087
Gavin Carr <gavin@openfusion.com.au>
2010-09-29 16:13:58 +0100

    Update Merger to handle Net::SSHR::Result objects; update rdocs.

--- lib/net/sshr/merger.rb
+++ lib/net/sshr/merger.rb
@@ -2,24 +2,26 @@
 module Net
   class SSHR
 
-    # Net::SSHR::Merger class, collating and merging result sets
+    # Net::SSHR::Merger class, merging and tracking distinct result sets with their hosts
     class Merger < Array
+
+      # Merge the given result into our current set of results: 
+      # if it matches an existing result, we append result.host to match.host;
+      # if it matches no existing result, we append it to our result set.
       def merge(result)
-        res[:stdout] ||= ''
-        res[:stdout].chomp!
-        res[:stderr] ||= ''
-        res[:stderr].chomp!
+        stdout = result.stdout
+        stderr = result.stderr
 
         added = false
         each do |merged|
-          if merged[:stdout] == res[:stdout] and merged[:stderr] == res[:stderr]:
-            merged[:host] += " #{res[:host]}"
+          if merged.stdout == stdout and merged.stderr == stderr:
+            merged.host += ' ' + result.host
             added = true
             break
           end
         end
 
-        push res if not added
+        push result if not added
       end
     end
   end
