d45b0ceb7b9bd019aa826b0130cec97e03ae5d99
Gavin Carr <gavin@openfusion.com.au>
2010-10-01 21:00:11 +0100

    Refactor sshr_exec to use gen_channel_exec_block infrastructure.

--- lib/net/sshr.rb
+++ lib/net/sshr.rb
@@ -31,7 +31,7 @@ module Net
       result_data = {}
 
       Net::SSH::Multi.start(:on_error => :warn) do |session|
-        # Define users and servers to connect to, and initialise result_data
+        # Setup server connections and result objects
         hosts.each do |host|
           # TODO: figure how to do user + ssh options stuff properly
           if (host =~ /@/):
@@ -41,48 +41,16 @@ module Net
             session_host = "root@#{host}"
             hostname = host
           end
-          session.use(session_host)
-          result_data[hostname] = Net::SSHR::Result.new(hostname, cmd)
+          server = session.use(session_host)
+          result_data[server.hash] = Net::SSHR::Result.new(hostname, cmd)
         end
 
         # Execute cmd on all servers
         session.open_channel do |channel|
-          # Callbacks to capture stdout and stderr
-          channel.on_data do |channel, data|
-            host = channel[:host]
-            result_data[host].stdout << data
-          end
-          channel.on_extended_data do |channel, type, data|
-            host = channel[:host]
-            result_data[host].stderr << data
-          end
-
-          # Callback to capture exit status
-          channel.on_request("exit-status") do |channel, data|
-            host = channel[:host]
-            result_data[host].exit_code = data.read_long
-          end
-
-          # Callback on channel close, yielding results
-          channel.on_close do |channel|
-            host = channel[:host]
-            if options[:verbose]:
-              $stderr.puts "+ channel close for host #{host}, yielding results"
-            end
-            yield result_data[host]
-          end
-
-          # Exec cmd on current channel
-          channel.exec cmd do |channel, success|
-            host = channel[:host]
-            if not success:
-              result_data[host].stderr << "exec on #{host} failed!"
-              result_data[host].exit_status ||= 255
-              yield result_data[host]
-            elsif options[:verbose]:
-              $stderr.puts "+ exec on host #{host} begun"
-            end
-          end
+          server = channel[:server]
+          result = result_data[server.hash]
+          exec_block = gen_channel_exec_block(result, options, &block)
+          channel.exec(cmd, &exec_block)
         end
 
         # Run the event loop
@@ -154,6 +122,8 @@ module Net
         session.loop
       end
     end
+
+    private
 
     def gen_channel_exec_block(result, options, &block)
       return lambda { |channel, success|
