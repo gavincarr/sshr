018debd429acd38c2fd03226da25ca64a97f2823
Gavin Carr <gavin@openfusion.com.au>
2010-12-07 21:14:46 +0000

    Update unit tests to test user handling, add test non-block result list.

--- test/test_net_sshr_exec.rb
+++ test/test_net_sshr_exec.rb
@@ -5,25 +5,36 @@ include Net::SSHR
 
 class TestNetSSHR < Test::Unit::TestCase
   def test_exec
-    # TODO: mock this, rather than requiring a localhost with date(1)
-    sshr_exec(%w{localhost localhost localhost}, 'date') do |result|
+    # TODO: mock this, rather than requiring a localhost with whoami(1)
+    sshr_exec(%w{localhost localhost localhost}, 'whoami') do |result|
       assert_not_nil result
       assert_kind_of Net::SSHR::Result, result
       assert_respond_to result, :stdout
       assert_respond_to result, :stderr
       assert_respond_to result, :exit_code
       assert_equal 'localhost', result.host
-      assert_match /^\w{3}\s+\w{3}\s+\d+/, result.stdout
+      assert_equal ENV['USER'], result.stdout.chomp!
       assert_equal '', result.stderr
       assert_equal 0, result.exit_code
     end
   end
+  def test_nonblock_exec_single
+    result_list = sshr_exec('root@localhost', 'whoami')
+    assert_not_nil result_list
+    assert_kind_of Array, result_list
+    assert_kind_of Net::SSHR::Result, result_list[0]
+    assert_equal 1, result_list.length
+    assert_equal 0, result_list[0].exit_code
+    assert_equal 'root', result_list[0].stdout.chomp!
+  end
   def test_nonblock_exec
-    result = sshr_exec('localhost', 'date')
-    assert_not_nil result
-    assert_kind_of Net::SSHR::Result, result
-    assert_equal 0, result.exit_code
-    assert_match /^\w{3}\s+\w{3}\s+\d+/, "#{result}"
+    result_list = sshr_exec(%w{root@localhost localhost}, 'whoami')
+    assert_not_nil result_list
+    assert_kind_of Array, result_list
+    assert_kind_of Net::SSHR::Result, result_list[0]
+    assert_equal 2, result_list.length
+    assert_equal 0, result_list[0].exit_code
+    assert_equal 'root', result_list[0].stdout.chomp!
+    assert_equal ENV['USER'], result_list[1].stdout.chomp!
   end
 end
-
