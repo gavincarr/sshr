067f6493ab181c3ca531c8d048b0acb7107c6526
Gavin Carr <gavin@openfusion.com.au>
2010-12-14 21:24:05 +0000

    Add option format conflict test to sshr, and unit tests to test_sshr.rb.

--- test/test_sshr.rb
+++ test/test_sshr.rb
@@ -1,7 +1,40 @@
 # sshr tests
 
 require 'test/unit'
+require 'open3'
 
 class TestSSHR < Test::Unit::TestCase
+  def setup
+    @dir = File.join(Dir.getwd, File.dirname($0))
+    @sshr = File.join(@dir, '..', 'bin', 'sshr')
+  end
+  def test_option_conflicts
+    format_conflict = %r{^Error: only one format option may be specified}
+    outerr_conflict = %r{^Error: only one stdout/stderr option may be specified}
+
+    cmd = "#{@sshr} --long --short localhost whoami"
+    Open3.popen3(cmd) do |stdin, stdout, stderr|
+      assert_equal(nil, stdout.gets(nil))
+      assert_match(format_conflict, stderr.gets(nil))
+    end
+
+    cmd = "#{@sshr} --short --list localhost whoami"
+    Open3.popen3(cmd) do |stdin, stdout, stderr|
+      assert_equal(nil, stdout.gets(nil))
+      assert_match(format_conflict, stderr.gets(nil))
+    end
+
+    cmd = "#{@sshr} --out --stderr localhost whoami"
+    Open3.popen3(cmd) do |stdin, stdout, stderr|
+      assert_equal(nil, stdout.gets(nil))
+      assert_match(outerr_conflict, stderr.gets(nil))
+    end
+
+    cmd = "#{@sshr} --oex --oeb localhost whoami"
+    Open3.popen3(cmd) do |stdin, stdout, stderr|
+      assert_equal(nil, stdout.gets(nil))
+      assert_match(outerr_conflict, stderr.gets(nil))
+    end
+  end
 end
 
